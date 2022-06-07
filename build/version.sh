a68e818c800f1997bc9a91106c46c06128c97cf2
David Eadie <david@eadie.net>
2022-06-07 12:46:24 +0100

    Add logging to version script

--- build/version.sh
+++ build/version.sh
@@ -17,17 +17,21 @@ HEAD_HASH=$(git rev-parse --verify HEAD)
 TAG_HASH=$(git log -1 --format=format:"%H" "$LATEST_TAG" 2>/dev/null | tail -n1)
 
 if [[ -z "$LATEST_TAG" ]]; then
+    >&2 echo "No previous versions found"
+    >&2 echo "Calculated version: $NEXT_VERSION.0"
     echo "$NEXT_VERSION.0"
-    exit
-fi
-
-if [[ "$HEAD_HASH" == "$TAG_HASH" ]]; then
-    echo "$LATEST_TAG"
     exit
 fi
 
 if [[ -n "$TAG_PREFIX" ]]; then
     LATEST_TAG=${LATEST_TAG#"$TAG_PREFIX"}
+fi
+
+if [[ "$HEAD_HASH" == "$TAG_HASH" ]]; then
+    >&2 echo "No changes since previous version"
+    >&2 echo "Calculated version: $LATEST_TAG"
+    echo "$LATEST_TAG"
+    exit
 fi
 
 # Read Version parts
@@ -39,13 +43,16 @@ LATEST_MINOR="${VERSION_PARTS[1]}"
 LATEST_PATCH="${VERSION_PARTS[2]}"
 
 if [[ "$MAJOR" > "$LATEST_MAJOR" ]]; then
+    >&2 echo "Calculated version: ${MAJOR}.${MINOR}.${PATCH}"
     echo "${MAJOR}.${MINOR}.${PATCH}"
     exit
 elif [[ "$MINOR" > "$LATEST_MINOR" ]]; then
+    >&2 echo "Calculated version: ${MAJOR}.${MINOR}.${PATCH}"
     echo "${MAJOR}.${MINOR}.${PATCH}"
     exit
 else
     ((LATEST_PATCH++))
+    >&2 echo "Calculated version: ${MAJOR}.${MINOR}.${LATEST_PATCH}"
     echo "${MAJOR}.${MINOR}.${LATEST_PATCH}"
     exit
 fi
--- makefile
+++ makefile
@@ -1,15 +1,15 @@
-IMAGE_NAME = theeadie/nest-exporter
-PLATFORMS = linux/amd64,linux/arm64
-NEXT_VERSION = 0.2
-TAG_PREFIX = 
+IMAGE_NAME := theeadie/nest-exporter
+PLATFORMS := linux/amd64,linux/arm64
+NEXT_VERSION := 0.2
+TAG_PREFIX := 
 
-GITHUB_REPO = theeadie/nest-exporter
-GITHUB_AUTH_TOKEN = empty
+GITHUB_REPO := theeadie/nest-exporter
+GITHUB_AUTH_TOKEN := empty
 
-VERSION = $(shell ./build/version.sh $(NEXT_VERSION))
-VERSION_MAJOR = $(word 1,$(subst ., ,$(VERSION)))
-VERSION_MINOR = $(word 2,$(subst ., ,$(VERSION)))
-VERSION_PATCH = $(word 2,$(subst ., ,$(VERSION)))
+VERSION := $(shell ./build/version.sh $(NEXT_VERSION))
+VERSION_MAJOR := $(word 1,$(subst ., ,$(VERSION)))
+VERSION_MINOR := $(word 2,$(subst ., ,$(VERSION)))
+VERSION_PATCH := $(word 2,$(subst ., ,$(VERSION)))
 
 .PHONY: default build
 
