4ab8b97b9641e738421ce61f7f2f9c8e851fc0d9
David Eadie <david@eadie.net>
2022-06-10 16:32:56 +0100

    Add tests for happy path of getting each stat

--- src/nest-exporter.tests/GlobalUsings.cs
+++ src/nest-exporter.tests/GlobalUsings.cs
@@ -1,3 +1,6 @@
 // Global using directives
 
 global using NUnit.Framework;
+
+[assembly: CLSCompliant(false)]
+namespace NestExporter.Tests;
--- src/nest-exporter.tests/Nest/NestClientShould.cs
+++ src/nest-exporter.tests/Nest/NestClientShould.cs
@@ -21,6 +21,28 @@ public class ServiceShould
     }
 
     [Test]
+    public async Task ReturnNameWhenReceivedFromNest()
+    {
+        using var message = new MockHttpMessageHandler(
+            @"{
+        ""devices"": [
+        {
+            ""name"" : ""enterprises/project-id/devices/device-id"",
+            ""traits"": {
+                ""sdm.devices.traits.Info"" : {
+                    ""customName"" : ""My device""
+                }
+            }
+        }]}",
+            HttpStatusCode.OK);
+        using var httpClient = new HttpClient(message);
+        _ = _httpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
+
+        var result = await _nestClient.GetThermostatInfo().ConfigureAwait(false);
+        result.Name.ShouldBe("My device");
+    }
+
+    [Test]
     public async Task ReturnTemperatureWhenReceivedFromNest()
     {
         using var message = new MockHttpMessageHandler(
@@ -38,8 +60,73 @@ public class ServiceShould
         using var httpClient = new HttpClient(message);
         _ = _httpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
 
-
         var result = await _nestClient.GetThermostatInfo().ConfigureAwait(false);
         result.ActualTemp.ShouldBe(23);
+    }
+
+    [Test]
+    public async Task ReturnHumidityWhenReceivedFromNest()
+    {
+        using var message = new MockHttpMessageHandler(
+            @"{
+        ""devices"": [
+        {
+            ""name"" : ""enterprises/project-id/devices/device-id"",
+            ""traits"" : {
+                ""sdm.devices.traits.Humidity"" : {
+                    ""ambientHumidityPercent"" : 35.0
+                }
+            }
+        }]}",
+            HttpStatusCode.OK);
+        using var httpClient = new HttpClient(message);
+        _ = _httpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
+
+        var result = await _nestClient.GetThermostatInfo().ConfigureAwait(false);
+        result.Humidity.ShouldBe(35);
+    }
+
+    [Test]
+    public async Task ReturnTargetTemperatureWhenReceivedFromNest()
+    {
+        using var message = new MockHttpMessageHandler(
+            @"{
+        ""devices"": [
+        {
+            ""name"" : ""enterprises/project-id/devices/device-id"",
+            ""traits"" : {
+                ""sdm.devices.traits.ThermostatTemperatureSetpoint"" : {
+                    ""heatCelsius"" : 23.0
+                }
+            }
+        }]}",
+            HttpStatusCode.OK);
+        using var httpClient = new HttpClient(message);
+        _ = _httpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
+
+        var result = await _nestClient.GetThermostatInfo().ConfigureAwait(false);
+        result.TargetTemp.ShouldBe(23);
+    }
+
+    [Test]
+    public async Task ReturnStatusWhenReceivedFromNest()
+    {
+        using var message = new MockHttpMessageHandler(
+            @"{
+        ""devices"": [
+        {
+            ""name"" : ""enterprises/project-id/devices/device-id"",
+            ""traits"" : {
+                ""sdm.devices.traits.ThermostatHvac"" : {
+                    ""status"" : ""HEATING""
+                }
+            }
+        }]}",
+            HttpStatusCode.OK);
+        using var httpClient = new HttpClient(message);
+        _ = _httpClientFactory.CreateClient(Arg.Any<string>()).Returns(httpClient);
+
+        var result = await _nestClient.GetThermostatInfo().ConfigureAwait(false);
+        result.Status.ShouldBe("HEATING");
     }
 }
